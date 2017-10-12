51a2a2d07cc40bf6e0255b345446faa67df93e77
juanjosevega99 <juanjosevegaquintero@hotmail.com>
2017-10-12 16:57:27 -0500

    Proyecyo completo de templates de django

---
+++ djangoTemplates/gatos/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Gato(models.Model):
+	name = models.CharField(max_length=255)
+	patas = models.IntegerField(default=4)
+
+	def __str__(self):
+		return self.name
